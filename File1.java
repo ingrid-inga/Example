ddd99bf33146e808e946fc59ff077b97abcab484
ingrid-inga <leidyingrid20@gmail.com>
2021-10-20 19:21:08 -0300

    conflict resolved

--- File1.java
+++ File1.java
@@ -2,11 +2,7 @@
 
 public class File1 {
 
-<<<<<<< HEAD
-    private String Lastname;
-=======
     private float name;
->>>>>>> fc847cb6ea890aacb2600a755d0587f080b66979
     private Integer document;
     
 }
